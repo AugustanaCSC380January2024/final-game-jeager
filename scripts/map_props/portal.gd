4b843cc94196cc56a33dfd7bdd2ffd127c123ede
bibhulamichhane <lamichhanebibhu0@gmail.com>
2024-01-26 13:43:10 -0600

    portal now transports you to the next level

--- scripts/map_props/portal.gd
+++ scripts/map_props/portal.gd
@@ -1,6 +1,26 @@
 extends Area2D
 
 @export var next_scene: PackedScene
+@onready var sprite = $AnimatedSprite2D
+@onready var label = $Label
 
+var portal_unlocked = false
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	label.visible = false
+	
 func column_hit():
-	$AnimatedSprite2D.frame += 1
+	sprite.frame += 1
+	if sprite.frame == 4:
+		portal_unlocked = true
+		sprite.frame += 1
+
+func _on_body_entered(body):
+	if portal_unlocked:
+		get_tree().change_scene_to_packed(next_scene)
+	else:
+		label.visible = true
+		await get_tree().create_timer(2).timeout
+		label.visible = false
+
