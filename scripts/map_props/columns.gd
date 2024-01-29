7a614d4e10238078b1f8ec8b4dc1e8096e33814c
Drishtantbhandari <Drishtantbhandari0@gmail.com>
2024-01-29 02:31:25 -0600

    Added lightnodes in objects

--- scripts/map_props/columns.gd
+++ scripts/map_props/columns.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var strucked = false
 signal Strucked
+@onready var point_light_2d = $PointLight2D
+
 
 func _on_body_entered(body):
 	print(body)
@@ -9,5 +11,6 @@ func _on_body_entered(body):
 func take_damage(damage):
 	if !strucked:
 		strucked = true
+		$PointLight2D.visible = true
 		$AnimatedSprite2D.play("pillar_strucked")
 		Strucked.emit()
