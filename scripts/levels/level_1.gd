61fb9d01112d550e7d2f027c4075676d80e96290
Drishtantbhandari <Drishtantbhandari0@gmail.com>
2024-01-29 01:24:20 -0600

    added pop up texts and fixed collision layers in map

--- scripts/levels/level_1.gd
+++ scripts/levels/level_1.gd
@@ -9,6 +9,22 @@ var tutorial = true
 	#new_dart.change_projectile_direction(dart_direction)
 	#new_dart.global_position = $Enemies/Yassop/dart_spawn_location.global_position
 
+#@onready var movement_collision_shape_2d = $tutorial/movement/CollisionShape2D
+#@onready var switch_collision_shape_2d = $tutorial/switch/CollisionShape2D
+#@onready var attack_collision_shape_2d = $tutorial/attack/CollisionShape2D
+#@onready var ult_collision_shape_2d = $tutorial/ult/CollisionShape2D
+#@onready var areasplash_collision_shape_2d = $tutorial/areasplash/CollisionShape2D
+#@onready var slime_attack_collision_shape_2d = $tutorial/slimeattack/CollisionShape2D
+
+
+@onready var movement_label: Label = $tutorial/movement/Label
+@onready var switch_label : Label = $tutorial/switch/Label
+@onready var attack_label : Label = $tutorial/attack/Label
+@onready var areasplash_label : Label = $tutorial/areasplash/Label
+@onready var ult_label : Label = $tutorial/ult/Label
+@onready var slimeattack_label : Label = $tutorial/slimeattack/Label
+@onready var hint1_label: Label = $tutorial/hint1/Label
+@onready var rune_label: Label = $tutorial/rune/Label
 
 func _process(delta):
 	if tutorial == true:
@@ -25,3 +41,68 @@ func _process(delta):
 			tutorial = false
 		#companion = companion_instantiated
 		
+
+
+func show_label(label: Label, text: String) -> void:
+	label.text = text
+	label.visible = true
+	
+func hide_label(label: Label) -> void:
+	label.visible = false
+	
+
+func _on_movement_body_entered(body: Node) -> void:
+	show_label(movement_label, "Use W, A, S, D, or arrow keys to move")
+func _on_movement_body_exited(body: Node) -> void:
+	hide_label(movement_label)
+
+
+func _on_switch_body_entered(body):
+	show_label(switch_label, "Press 1, 2,3 to switch between characters")
+func _on_switch_body_exited(body):
+	hide_label(switch_label)
+	
+
+
+func _on_attack_body_entered(body):
+	show_label(attack_label, "Press spacebar to attack the slime")
+func _on_attack_body_exited(body):
+	hide_label(attack_label)
+
+func _on_areasplash_body_entered(body):
+	show_label(areasplash_label, "Switch to miko and attack to 
+	deal damage to multiple
+	 enemies at once")
+func _on_areasplash_body_exited(body):
+	hide_label(areasplash_label)
+
+
+func _on_ult_body_entered(body):
+	show_label(ult_label, "Press E to ult. Be mindful of the 
+	cooldown on the bottom right!")
+func _on_ult_body_exited(body):
+	hide_label(ult_label)
+
+
+func _on_slimeattack_body_entered(body):
+	show_label(slimeattack_label, "save the outcast slime")
+func _on_slimeattack_body_exited(body):
+	hide_label(slimeattack_label)
+
+
+func _on_areasplash_2_body_entered(body):
+	show_label(hint1_label,"Switch characters between
+	ult cooldown to deal more 
+	damage per second")
+
+
+func _on_areasplash_2_body_exited(body):
+	hide_label(hint1_label)
+
+
+func _on_rune_body_entered(body):
+	show_label(rune_label,"Hit the rune to activate it")
+
+
+func _on_rune_body_exited(body):
+	hide_label(rune_label)
